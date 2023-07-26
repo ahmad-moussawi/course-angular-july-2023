e9cddcc84d667d97118ab81d52a2e0bc7fb7fb86
Ahmad Moussawi <aymoussawi@gmail.com>
2023-07-26 14:20:05 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<app-nav />`,
+  styles: [``],
+})
+export class AppComponent {
+  title = 'FitnessStore';
+}
